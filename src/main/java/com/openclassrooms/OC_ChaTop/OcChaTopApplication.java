f50c1f663dc235b85cbc921ac1180ab85b9c8b89
leop <leopetit200@gmail.com>
2024-09-06 10:58:51 +0200

    first commit

---
+++ src/main/java/com/openclassrooms/OC_ChaTop/OcChaTopApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.OC_ChaTop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcChaTopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcChaTopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/OC_ChaTop/OcChaTopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.OC_ChaTop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcChaTopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
